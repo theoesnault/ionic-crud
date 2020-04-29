a6a889bdb72437c3b6466d25e0275f6ada3c5cb5
Théo Esnault <theo.esnault@gmail.com>
2020-04-29 16:02:05 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  {path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'add', loadChildren: () => import('./add/add.module').then( m => m.AddPageModule)},
+  {path: 'edit', loadChildren: () => import('./edit/edit.module').then( m => m.EditPageModule)},
 ];
 
 @NgModule({
